0de8679bc74ce6a49b9afcb4d47d4dc4723bbecc
Daniel Sherlock <d.t.sherlock@live.co.uk>
2020-04-13 21:53:06 +0100

    First try at full lobby

--- script.js
+++ script.js
@@ -3,21 +3,40 @@ function switchFromTo(from, to) {
   document.getElementById(to).style.display = 'block';
 }
 
-function startGame() {
-  // pass
+function startGame(conn) {
+  return function() {// pass};
+}
+
+function lobbyMainMenu() {
+  switchFromTo('nickname', 'lobby-mainmenu');
 }
 
 function lobbyStart() {
-  var peer = new Peer();
+  const peer = new Peer();
   peer.on('open', function(id) {
     document.getElementById('player-code').innerHTML = id;
   });
-  peer.on('connection', startGame);
+  peer.on('connection', function(conn) {
+    conn.on('open', startGame(conn));
+  };
   switchFromTo('lobby-mainmenu', 'lobby-start');
 }
 
+function lobbyJoin() {
+  switchFromTo('lobby-mainmenu', 'lobby-join');
+}
+
+function makeConnection(){
+  const peer = new Peer();
+  const conn = peer.connect(document.getElementById('player-code-text').value);
+  conn.on('open', startGame(conn));
+}
+
 function main() {
+  document.getElementById('nickname-button').addEventListener('click', lobbyMainMenu);
   document.getElementById('start-button').addEventListener('click', lobbyStart);
+  document.getElementById('join-button').addEventListener('click', lobbyJoin);
+  document.getElementById('player-code-button').addEventListener('click', makeConnection);
 }
 
 if (document.readyState === 'loading') {
