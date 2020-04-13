3e89f67d40435f60d4ab37782a0330245c1dc5df
Daniel Sherlock <d.t.sherlock@live.co.uk>
2020-04-13 20:57:33 +0100

    Testing events

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function main() {
+function peerStuff(){
   var peer = new Peer();
   peer.on('open', function(id) {
     alert('My peer ID is: ' + id);
   });
+}
+
+function test() {
+  alert("seems to work fine");
+}
+
+function main() {
+  document.getElementById('start-button').addEventListener('click', test);
 }
 
 if (document.readyState === 'loading') {
