3d7d608ee7390582c2ed38d2d15a3e81bfea4215
Daniel Sherlock <d.t.sherlock@live.co.uk>
2020-04-13 22:02:26 +0100

    Add switch to game

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function switchFromTo(from, to) {
   document.getElementById(to).style.display = 'block';
 }
 
-function startGame(conn) {
+function startGame(conn, from) {
   return function() {
-    // pass
+    switchFromTo(from, 'game');
   };
 }
 
@@ -19,7 +19,7 @@ function lobbyStart() {
     document.getElementById('player-code').innerHTML = id;
   });
   peer.on('connection', function(conn) {
-    conn.on('open', startGame(conn));
+    conn.on('open', startGame(conn, 'lobby-start'));
   });
   switchFromTo('lobby-mainmenu', 'lobby-start');
 }
@@ -31,7 +31,7 @@ function lobbyJoin() {
 function makeConnection(){
   const peer = new Peer();
   const conn = peer.connect(document.getElementById('player-code-text').value);
-  conn.on('open', startGame(conn));
+  conn.on('open', startGame(conn, 'lobby-join'));
 }
 
 function main() {
