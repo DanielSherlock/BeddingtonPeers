8b8a278da7e07e650dcc329ac7c5f37dff3e063d
Daniel Sherlock <d.t.sherlock@live.co.uk>
2020-04-13 21:16:50 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,16 +1,23 @@
-function peerStuff(){
-  var peer = new Peer();
-  peer.on('open', function(id) {
-    alert('My peer ID is: ' + id);
-  });
+function switchFromTo(from, to) {
+  document.getElementById(from).style.display = 'none';
+  document.getElementById(to).style.display = 'block';
 }
 
-function test() {
-  alert("seems to work fine");
+function startGame() {
+  // pass
+}
+
+function lobbyStart() {
+  var peer = new Peer();
+  peer.on('open', function(id) {
+    document.getElementById('player-code').innerHTML = id;
+  });
+  peer.on('connection', startGame);
+  switchFromTo('lobby-mainmenu', 'lobby-start');
 }
 
 function main() {
-  document.getElementById('start-button').addEventListener('click', test);
+  document.getElementById('start-button').addEventListener('click', lobbyStart);
 }
 
 if (document.readyState === 'loading') {
