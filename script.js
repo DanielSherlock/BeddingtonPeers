428cab46cd7863b05ac62067efa3765ec9e89ac0
Daniel Sherlock <d.t.sherlock@live.co.uk>
2020-04-16 16:53:53 +0100

    Local-ised script.js for now

--- script.js
+++ script.js
@@ -3,8 +3,18 @@ function switchFromTo(from, to) {
   document.getElementById(to).style.display = 'block';
 }
 
-function startGame(peer, conn, from) {
+// Local functions:
+
+function startLocalGame() {
+  switchFromTo('local-menu', 'game');
+  alert('Alrighty then!');
+}
+
+// Non-working PeerJS functions:
+
+function startPeerJSGame(peer, conn, from) {
   return function() {
+    alert('If you see this, Peer JS does seem to work - tell me immediately!');
     console.log(peer);
     console.log(conn);
     switchFromTo(from, 'game');
@@ -26,7 +36,7 @@ function lobbyStart() {
   peer.on('connection', function(conn) {
     conn.on('open', function() { // this is just for testing
       alert('Connected!');
-      startGame(peer, conn, 'lobby-start');
+      startPeerJSGame(peer, conn, 'lobby-start');
     });
   });
   switchFromTo('lobby-mainmenu', 'lobby-start');
@@ -44,15 +54,20 @@ function makeConnection(){
   var conn = peer.connect(document.getElementById('player-code-text').value);
   conn.on('open', function() { // this is just for testing
     alert('Connected!');
-    startGame(peer, conn, 'lobby-join');
+    startPeerJSGame(peer, conn, 'lobby-join');
   });
 }
 
+// Good stuff:
+
 function main() {
+  // Non-working PeerJS buttons and stuff:
   document.getElementById('nickname-button').addEventListener('click', lobbyMainMenu);
   document.getElementById('start-button').addEventListener('click', lobbyStart);
   document.getElementById('join-button').addEventListener('click', lobbyJoin);
   document.getElementById('player-code-button').addEventListener('click', makeConnection);
+  // Local
+  document.getElementById('local-start-button').addEventListener('click', startLocalGame);
 }
 
 if (document.readyState === 'loading') {
